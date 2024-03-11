391332e39ef5ed81ec421cae0f0e0a868e8e9fab
mhid-rethink <matheus.dias@rethink.dev>
2024-03-11 20:31:54 -0300

    feat: ModalContext

---
+++ src/contexts/ModalContext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from "react";
+
+const ModalContext = createContext();
+
+export const ModalProvider = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  return (
+    <ModalContext.Provider value={{ isOpen, openModal, closeModal }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export const useModal = () => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error("useModal must be used within a ModalProvider");
+  }
+  return context;
+};
