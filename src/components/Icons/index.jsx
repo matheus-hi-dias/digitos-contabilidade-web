89bfc2dfebe0a65cc8b8ad6a38a5cfb7e584a85f
mhid-rethink <matheus.dias@rethink.dev>
2024-03-20 21:10:48 -0300

    style: loginScreen

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -1,2 +1,3 @@
 export { FaEye, FaEdit } from "react-icons/fa";
+export { FaUser } from "react-icons/fa6";
 export { MdDelete } from "react-icons/md";
--- src/screens/loginScreen/index.jsx
+++ src/screens/loginScreen/index.jsx
@@ -1,18 +1,29 @@
-import { Button } from "../../components";
+import { Button, FaUser } from "../../components";
+import "./styles.scss";
 const LoginScreen = () => {
- return (
-  <div>
-    <div><h1>Login</h1>
-      <form>
-        <div>
-          <input type="text" name="username" id="username" />
-          <input type="password" name="password" id="password" />
-          <Button variant={"primaryButton"}/>
-        </div>
-      </form>
+  return (
+    <div className="loginScreen">
+      <div className="loginContainer">
+        <h1>Login</h1>
+        <FaUser className="loginIcon" size={136} />
+        <form>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Usuário ou e-mail"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Senha"
+          />
+          <Button variant={"primaryButton"} type="button" text={"Entrar"} buttonCustomClass="loginButton"/>
+        </form>
+      </div>
     </div>
-  </div>
- ) 
-}
+  );
+};
 
 export default LoginScreen;
