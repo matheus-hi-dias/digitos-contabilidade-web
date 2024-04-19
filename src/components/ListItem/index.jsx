f7218368d8181e0c0698151b2eb06dcfee127f5f
Matheus Dias <hmatheus031@gmail.com>
2024-04-19 18:28:28 -0300

    ref ListItem

--- src/components/ListItem/index.jsx
+++ src/components/ListItem/index.jsx
@@ -6,15 +6,31 @@ import Button from '../Button';
 import './styles.scss';
 
 function ListItem({
-  description, seeAction, updateAction, deleteAction,
+  description,
+  seeAction,
+  updateAction,
+  deleteAction,
+  seeButton = true,
+  updateButton = true,
+  deleteButton = true,
 }) {
   return (
     <div className="listItemContainer">
       <span className="itemDescription">{description}</span>
       <span className="itemActions">
-        <Button data-testid="see-button" buttonCustomClass="itemActionButton" icon={<FaEye size={20} />} onClick={seeAction} />
-        <Button data-testid="update-button" buttonCustomClass="itemActionButton" icon={<FaEdit size={20} />} onClick={updateAction} />
-        <Button data-testid="delete-button" buttonCustomClass="itemActionButton" icon={<MdDelete size={20} />} onClick={deleteAction} />
+        {
+          seeButton
+          && (<Button data-testid="see-button" buttonCustomClass="itemActionButton" icon={<FaEye size={20} />} onClick={seeAction} />)
+        }
+        {
+          updateButton
+        && (<Button data-testid="update-button" buttonCustomClass="itemActionButton" icon={<FaEdit size={20} />} onClick={updateAction} />)
+        }
+        {
+          deleteButton && (
+            <Button data-testid="delete-button" buttonCustomClass="itemActionButton" icon={<MdDelete size={20} />} onClick={deleteAction} />
+          )
+        }
       </span>
     </div>
   );
