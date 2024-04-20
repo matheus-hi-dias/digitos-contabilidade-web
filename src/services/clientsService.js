1695bff29c00ef5b0e4e7df0f62d67ae9a4d5d40
mhid-rethink <matheus.dias@rethink.dev>
2024-04-20 20:14:04 -0300

    ref services

--- src/services/clientsService.js
+++ src/services/clientsService.js
@@ -1,47 +1,26 @@
 import { api, configHeader } from './api';
 
 export async function getClients() {
-  try {
-    const response = await api.get('/clients', configHeader());
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+  const response = await api.get('/clients', configHeader());
+  return response.data;
 }
 
 export async function getClientById(id) {
-  try {
-    const response = await api.get(`/clients/${id}`, configHeader());
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+  const response = await api.get(`/clients/${id}`, configHeader());
+  return response.data;
 }
 
 export async function updateClient(id, data) {
-  try {
-    const response = await api.put(`/clients/${id}`, data, configHeader());
-    return response;
-  } catch (error) {
-    return error.response;
-  }
+  const response = await api.put(`/clients/${id}`, data, configHeader());
+  return response.data;
 }
 
 export async function createClient(data) {
-  try {
-    const response = await api.post('/clients', data, configHeader());
-    return response;
-  } catch (error) {
-    console.log(error.response);
-    return error.response;
-  }
+  const response = await api.post('/clients', data, configHeader());
+  return response.data;
 }
 
 export async function deleteClient(id) {
-  try {
-    const response = await api.delete(`/clients/${id}`, configHeader());
-    return response;
-  } catch (error) {
-    return error.response;
-  }
+  const response = await api.delete(`/clients/${id}`, configHeader());
+  return response.data;
 }
--- src/services/documentsNature.js
+++ src/services/documentsNature.js
@@ -1,47 +1,26 @@
 import { api, configHeader } from './api';
 
 export async function getNatures() {
-  try {
-    const response = await api.get('/nature', configHeader());
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+  const response = await api.get('/nature', configHeader());
+  return response.data;
 }
 
 export async function getNatureById(id) {
-  try {
-    const response = await api.get(`/nature/${id}`, configHeader());
-    return response.data;
-  } catch (error) {
-    return error.response;
-  }
+  const response = await api.get(`/nature/${id}`, configHeader());
+  return response.data;
 }
 
 export async function updateNature(id, data) {
-  try {
-    const response = await api.put(`/nature/${id}`, data, configHeader());
-    return response;
-  } catch (error) {
-    console.log(error.response);
-    return error.response;
-  }
+  const response = await api.put(`/nature/${id}`, data, configHeader());
+  return response;
 }
 
 export async function createNature(data) {
-  try {
-    const response = await api.post('/nature', data, configHeader());
-    return response;
-  } catch (error) {
-    return error.response;
-  }
+  const response = await api.post('/nature', data, configHeader());
+  return response;
 }
 
 export async function deleteNature(id) {
-  try {
-    const response = await api.delete(`/nature/${id}`, configHeader());
-    return response;
-  } catch (error) {
-    return error.response;
-  }
+  const response = await api.delete(`/nature/${id}`, configHeader());
+  return response;
 }
