96935c3745f442cb661ed1b007e042f62b7f8c16
Matheus Dias <hmatheus031@gmail.com>
2024-04-23 14:13:51 -0300

    fix: service document storage local

--- src/services/documentsStorageLocal.js
+++ src/services/documentsStorageLocal.js
@@ -1,26 +1,26 @@
 import { api, configHeader } from './api';
 
 export async function getDocumentStorageLocal() {
-  const response = await api.get('/document-storage-local', configHeader());
+  const response = await api.get('/document-location', configHeader());
   return response.data;
 }
 
 export async function getDocumentStorageLocalById(id) {
-  const response = await api.get(`/document-storage-local/${id}`, configHeader());
+  const response = await api.get(`/document-location/${id}`, configHeader());
   return response.data;
 }
 
 export async function updateDocumentStorageLocal(id, data) {
-  const response = await api.put(`/document-storage-local/${id}`, data, configHeader());
+  const response = await api.put(`/document-location/${id}`, data, configHeader());
   return response.data;
 }
 
 export async function createDocumentStorageLocal(data) {
-  const response = await api.post('/document-storage-local', data, configHeader());
+  const response = await api.post('/document-location', data, configHeader());
   return response.data;
 }
 
 export async function deleteDocumentStorageLocal(id) {
-  const response = await api.delete(`/document-storage-local/${id}`, configHeader());
+  const response = await api.delete(`/document-location/${id}`, configHeader());
   return response.data;
 }
