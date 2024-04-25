df2d29f5d97c6def8e5d68b3100aaa505bc32f0c
mhid-rethink <matheus.dias@rethink.dev>
2024-04-24 21:08:28 -0300

    feat: modals document type

--- src/screens/documentTypeScreen/index.jsx
+++ src/screens/documentTypeScreen/index.jsx
@@ -7,25 +7,37 @@ import {
   ListItem,
   Modal,
   SearchInput,
+  TextInput,
 } from '../../components';
 import './styles.scss';
-import { documentTypeList } from '../../constants/mocks';
+import {
+  createDocumentType, deleteDocumentType, getDocumentTypeById, getDocumentTypes, updateDocumentType,
+} from '../../services/documentsType';
 
 function DocumentTypeScreen() {
+  const [documentTypeList, setDocumentTypeList] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalChildren, setModalChildren] = useState(null);
+  const [isModalCreateOpen, setIsModalCreateOpen] = useState(false);
+  const [isModalSeeOpen, setIsModalSeeOpen] = useState(false);
+  const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
+  const [isModalDeleteOpen, setIsModalDeleteOpen] = useState(false);
   const [documentTypeData, setDocumentTypeData] = useState({
-    nature: '',
   });
 
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
+  useEffect(() => {
+    if (!isModalOpen) {
+      getDocumentTypes().then(setDocumentTypeList);
+    }
+  }, [isModalCreateOpen, isModalDeleteOpen, isModalUpdateOpen]);
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
+    setIsModalCreateOpen(false);
+    setIsModalSeeOpen(false);
+    setIsModalUpdateOpen(false);
+    setIsModalDeleteOpen(false);
     setDocumentTypeData({
-      nature: '',
+
     });
   };
 
@@ -36,23 +48,41 @@ function DocumentTypeScreen() {
     }));
   };
 
+  const handleCreateAction = async (event) => {
+    event.preventDefault();
+    await createDocumentType(documentTypeData);
+    handleCloseModal();
+  };
+
+  const handleUpdateAction = async (event) => {
+    event.preventDefault();
+    await updateDocumentType(documentTypeData.id, documentTypeData);
+    handleCloseModal();
+  };
+
+  const handleDeleteAction = async (event) => {
+    event.preventDefault();
+    await deleteDocumentType(documentTypeData.id);
+    handleCloseModal();
+  };
+
   const openCreateDocumentTypeModal = () => {
-    handleOpenModal();
-    setModalChildren(
+    if (!isModalCreateOpen) return null;
+    return (
       <>
         <label htmlFor="doc_type">
           Tipo de documento*:
-          <input type="text" name="doc_type" onChange={handleDocumentTypeData} />
+          <TextInput variant="formField" type="text" name="doc_type" onChange={handleDocumentTypeData} />
         </label>
         <label htmlFor="archiving_time">
           Tempo de armazenamento*:
-          <input type="text" name="archiving_time" onChange={handleDocumentTypeData} />
+          <TextInput variant="formField" type="text" name="archiving_time" onChange={handleDocumentTypeData} />
         </label>
         <div className="modalButtonsContainer">
           <Button
             variant="primaryButton"
             text="Cadastrar"
-            onClick={handleCloseModal}
+            onClick={handleCreateAction}
           />
           <Button
             variant="primaryButton"
@@ -60,23 +90,21 @@ function DocumentTypeScreen() {
             onClick={handleCloseModal}
           />
         </div>
-      </>,
+      </>
     );
   };
 
-  const openSeeDocumentTypeModal = (id) => {
-    const selectedDocumentTypeData = documentTypeList.find((item) => item.id === id);
-    setDocumentTypeData(selectedDocumentTypeData);
-    handleOpenModal();
-    setModalChildren(
+  const openSeeDocumentTypeModal = () => {
+    if (!isModalSeeOpen) return null;
+    return (
       <>
         <label htmlFor="doc_type">
           Tipo de documento*:
-          <input type="text" name="doc_type" disabled value={selectedDocumentTypeData.doc_type} />
+          <TextInput variant="formField" type="text" name="doc_type" disabled value={documentTypeData.doc_type} />
         </label>
         <label htmlFor="archiving_time">
           Tempo de armazenamento*:
-          <input type="text" name="archiving_time" disabled value={selectedDocumentTypeData.archiving_time} />
+          <TextInput variant="formField" type="text" name="archiving_time" disabled value={documentTypeData.archiving_time} />
         </label>
         <div className="modalButtonsContainer">
           <Button
@@ -85,29 +113,27 @@ function DocumentTypeScreen() {
             onClick={handleCloseModal}
           />
         </div>
-      </>,
+      </>
     );
   };
 
-  const openUpdateDocumentTypeModal = (id) => {
-    const selectedDocumentTypeData = documentTypeList.find((item) => item.id === id);
-    setDocumentTypeData(selectedDocumentTypeData);
-    handleOpenModal();
-    setModalChildren(
+  const openUpdateDocumentTypeModal = () => {
+    if (!isModalUpdateOpen) return null;
+    return (
       <>
         <label htmlFor="doc_type">
           Tipo de documento*:
-          <input type="text" name="doc_type" defaultValue={selectedDocumentTypeData.doc_type} onChange={handleDocumentTypeData} />
+          <TextInput variant="formField" type="text" name="doc_type" defaultValue={documentTypeData.doc_type} onChange={handleDocumentTypeData} />
         </label>
         <label htmlFor="archiving_time">
           Tempo de armazenamento*:
-          <input type="text" name="archiving_time" defaultValue={selectedDocumentTypeData.archiving_time} onChange={handleDocumentTypeData} />
+          <TextInput variant="formField" type="text" name="archiving_time" defaultValue={documentTypeData.archiving_time} onChange={handleDocumentTypeData} />
         </label>
         <div className="modalButtonsContainer">
           <Button
             variant="primaryButton"
             text="Alterar"
-            onClick={handleCloseModal}
+            onClick={handleUpdateAction}
           />
           <Button
             variant="primaryButton"
@@ -115,22 +141,22 @@ function DocumentTypeScreen() {
             onClick={handleCloseModal}
           />
         </div>
-      </>,
+      </>
     );
   };
 
-  useEffect(() => {}, [documentTypeData]);
+  useEffect(() => { console.log({ documentTypeData }); }, [documentTypeData]);
 
   const openDeleteDocumentTypeModal = () => {
-    handleOpenModal();
-    setModalChildren(
+    if (!isModalDeleteOpen) return null;
+    return (
       <>
         <h2>Deletar tipo de documento?</h2>
         <div className="modalButtonsContainer">
           <Button
             variant="primaryButton"
             text="Deletar"
-            onClick={handleCloseModal}
+            onClick={handleDeleteAction}
           />
           <Button
             variant="primaryButton"
@@ -138,7 +164,7 @@ function DocumentTypeScreen() {
             onClick={handleCloseModal}
           />
         </div>
-      </>,
+      </>
     );
   };
 
@@ -149,7 +175,10 @@ function DocumentTypeScreen() {
           variant="secondaryButton"
           icon={<AddIcon size={24} />}
           text="Adicionar"
-          onClick={openCreateDocumentTypeModal}
+          onClick={() => {
+            setIsModalOpen(true);
+            setIsModalCreateOpen(true);
+          }}
         />
         <SearchInput />
       </div>
@@ -158,13 +187,34 @@ function DocumentTypeScreen() {
           <ListItem
             description={item.doc_type}
             key={item.id}
-            seeAction={() => openSeeDocumentTypeModal(item.id)}
-            updateAction={() => openUpdateDocumentTypeModal(item.id)}
-            deleteAction={() => openDeleteDocumentTypeModal(item.id)}
+            seeAction={async () => {
+              setDocumentTypeData(await getDocumentTypeById(item.id));
+              setIsModalOpen(true);
+              setIsModalSeeOpen(true);
+            }}
+            updateAction={async () => {
+              setDocumentTypeData(await getDocumentTypeById(item.id));
+              setIsModalOpen(true);
+              setIsModalUpdateOpen(true);
+            }}
+            deleteAction={async () => {
+              setDocumentTypeData(await getDocumentTypeById(item.id));
+              setIsModalOpen(true);
+              setIsModalDeleteOpen(true);
+            }}
           />
         ))}
       </List>
-      {isModalOpen && <Modal onClose={handleCloseModal}>{modalChildren}</Modal>}
+      {isModalOpen && (
+      <Modal onClose={handleCloseModal}>
+        {
+        (isModalCreateOpen && (openCreateDocumentTypeModal()))
+        || (isModalSeeOpen && (openSeeDocumentTypeModal()))
+        || (isModalUpdateOpen && (openUpdateDocumentTypeModal()))
+        || (isModalDeleteOpen && (openDeleteDocumentTypeModal()))
+      }
+      </Modal>
+      )}
     </div>
   );
 }
