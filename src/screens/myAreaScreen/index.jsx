9dab9628c3cfa0f18287126dfcec1fb3df326051
Matheus Dias <hmatheus031@gmail.com>
2024-05-02 10:21:40 -0300

    feat: MyAreaScreen

--- src/screens/myAreaScreen/index.jsx
+++ src/screens/myAreaScreen/index.jsx
@@ -1,6 +1,71 @@
+import { useEffect, useState } from 'react';
+import './styles.scss';
+import { List, ListItem, TextInput } from '../../components';
+
+import permissions from '../../constants/permissions';
+import { getEmployeeProfile } from '../../services/employees';
+import { getPermissionsByEmployeeId } from '../../services/employeesPermission';
+import { getPermissionByRoleId } from '../../services/rolesPermission';
+
 function MyAreaScreen() {
+  const [myData, setMyData] = useState({});
+  const [myPermissions, setMyPermissions] = useState([]);
+  const [rolePermissions, setRolePermissions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getEmployeeProfile();
+      console.log({ response });
+      setMyData(response);
+
+      const myPermissionsResponse = await getPermissionsByEmployeeId(response.id);
+      console.log({ myPermissionsResponse });
+      setMyPermissions(myPermissionsResponse);
+
+      const rolePermissionsResponse = response.role?.id
+        ? await getPermissionByRoleId(response.role.id)
+        : [];
+      console.log({ rolePermissionsResponse });
+      setRolePermissions(rolePermissionsResponse);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div className="myAreaScreenLayout">Loading...</div>;
+  }
   return (
-    <div>Minha Área</div>
+    <div className="myAreaScreenLayout">
+      <div className="myAreaScreenContent">
+        <h1 className="myAreaTitle">{myData.name}</h1>
+        <label>
+          Cargo:
+          <TextInput variant="formField" type="text" disabled value={myData.role?.role || ''} />
+        </label>
+        <div className="permissionsDiv">
+          Permissões:
+          <List containerClassName="myPermissionsListContainer">
+            {rolePermissions.map((permission) => (
+              <ListItem
+                key={permission.id}
+                description={`${myData.role.role} - ${permissions[permission.permission]}`}
+                seeButton={false}
+                updateButton={false}
+                deleteButton={false}
+              />
+            ))}
+            {myPermissions.map((permission) => (
+              <ListItem
+                key={permission.id}
+                description={`${myData.name} - ${permissions[permission.permission]}`}
+              />
+            ))}
+          </List>
+        </div>
+      </div>
+    </div>
   );
 }
 
