6f841781e7389d8ab764a38af06dd6f891679053
Matheus Dias <hmatheus031@gmail.com>
2024-05-11 18:34:25 -0300

    feat: ResponsePopup component for displaying success and error messages

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -1,4 +1,6 @@
 export { FaEye, FaEdit } from 'react-icons/fa';
 export { FaUser } from 'react-icons/fa6';
-export { MdDelete, MdAddCircleOutline as AddIcon, MdClose as Close } from 'react-icons/md';
+export {
+  MdDelete, MdAddCircleOutline as AddIcon, MdClose as Close, MdCheck as Success,
+} from 'react-icons/md';
 export { BiSearch } from 'react-icons/bi';
